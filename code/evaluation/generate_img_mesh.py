965f7cc7d0e17aa9fbf45a4f95c02f931d2ed082
yuqun <yuqunwu2@illinois.edu>
2023-05-07 00:32:00 -0500

    add code from mesh to pc

--- code/evaluation/generate_img_mesh.py
+++ code/evaluation/generate_img_mesh.py
@@ -8,11 +8,59 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 import pandas as pd
+import open3d as o3d
+import multiprocessing as mp
 
 import utils.general as utils
 import utils.plots as plt
 from utils import rend_util
 from evaluation.evaluate_single_scene import cull_scan
+
+def sample_single_tri(input_):
+    n1, n2, v1, v2, tri_vert = input_
+    c = np.mgrid[:n1+1, :n2+1]
+    c += 0.5
+    c[0] /= max(n1, 1e-7)
+    c[1] /= max(n2, 1e-7)
+    c = np.transpose(c, (1,2,0))
+    k = c[c.sum(axis=-1) < 1]  # m2
+    q = v1 * k[:,:1] + v2 * k[:,1:] + tri_vert
+    return q
+
+def generate_pc_from_mesh(scan_id, mesh_path, pc_path, thresh):
+    mp.freeze_support()
+    pbar = tqdm(total=9)
+    pbar.set_description('read data mesh')
+    data_mesh = o3d.io.read_triangle_mesh(mesh_path)
+
+    vertices = np.asarray(data_mesh.vertices)
+    triangles = np.asarray(data_mesh.triangles)
+    tri_vert = vertices[triangles]
+
+    # pbar.update(1)
+    # pbar.set_description('sample pcd from mesh')
+    v1 = tri_vert[:,1] - tri_vert[:,0]
+    v2 = tri_vert[:,2] - tri_vert[:,0]
+    l1 = np.linalg.norm(v1, axis=-1, keepdims=True)
+    l2 = np.linalg.norm(v2, axis=-1, keepdims=True)
+    area2 = np.linalg.norm(np.cross(v1, v2), axis=-1, keepdims=True)
+    non_zero_area = (area2 > 0)[:,0]
+    l1, l2, area2, v1, v2, tri_vert = [
+        arr[non_zero_area] for arr in [l1, l2, area2, v1, v2, tri_vert]
+    ]
+    thr = thresh * np.sqrt(l1 * l2 / area2)
+    n1 = np.floor(l1 / thr)
+    n2 = np.floor(l2 / thr)
+
+    with mp.Pool() as mp_pool:
+        new_pts = mp_pool.map(sample_single_tri, ((n1[i,0], n2[i,0], v1[i:i+1], v2[i:i+1], tri_vert[i:i+1,0]) for i in range(len(n1))), chunksize=1024)
+
+    new_pts = np.concatenate(new_pts, axis=0)
+    data_pcd = np.concatenate([vertices, new_pts], axis=0)
+
+    point_cloud = o3d.geometry.PointCloud()
+    point_cloud.points = o3d.utility.Vector3dVector(data_pcd)
+    o3d.io.write_point_cloud(pc_path, point_cloud)
 
 def evaluate(**kwargs):
     torch.set_default_dtype(torch.float32)
@@ -24,6 +72,7 @@ def evaluate(**kwargs):
     results_folder_name = evals_folder_name + '/results'
     generate_image = kwargs['generate_image']
     generate_mesh = kwargs['generate_mesh']
+    thresh = kwargs['thresh']
 
     scan_id = kwargs['scan_id'] if kwargs['scan_id'] is not None else conf.get_int('dataset.scan_id', default=-1)
 
@@ -52,10 +101,10 @@ def evaluate(**kwargs):
     model.load_state_dict(saved_model_state["model_state_dict"], strict=True)
 
     ####################################################################################################################
-    print("evaluating...")
-
+   
     model.eval()
     if generate_mesh:
+        print("Generating mesh...")
         with torch.no_grad():
             grid_boundary=conf.get_list('plot.grid_boundary')
             mesh = plt.get_surface_sliding(path="",
@@ -85,9 +134,11 @@ def evaluate(**kwargs):
         # Cull point cloud from mesh
         # cull_scan(scan, mesh_path, result_mesh_file)
         pc_path = '{0}/{1}.ply'.format(results_folder_name, scan_id)
-        cull_scan(scan_id, mesh_path, pc_path)
+        # cull_scan(scan_id, mesh_path, pc_path)
+        generate_pc_from_mesh(scan_id, mesh_path, pc_path, thresh)
 
     if generate_image:
+        print("Generating image...")
         eval_dataloader = torch.utils.data.DataLoader(eval_dataset,
                                                 batch_size=1,
                                                 shuffle=False,
@@ -151,6 +202,7 @@ if __name__ == '__main__':
     parser.add_argument('--world_space', default=False, action="store_true", help='If set, transform to world space')
     parser.add_argument('--generate_image', default=True, type = eval, choices=[True, False], help='Whether generate image.')
     parser.add_argument('--generate_mesh', default=True, type = eval, choices=[True, False], help='Whether generate mesh.')
+    parser.add_argument('--downsample_density', default=0.1, type=float, help='Downsample density for mesh generation')
 
     opt = parser.parse_args()
 
@@ -162,4 +214,5 @@ if __name__ == '__main__':
              world_space=opt.world_space,
              generate_image=opt.generate_image,
              generate_mesh=opt.generate_mesh,
+             thresh = opt.downsample_density
              )
